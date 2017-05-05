82b00ef9f51dc4bc53988bf2d6b301ca39a748d3
roechi <msg.remus@gmail.com>
2017-05-05 08:54:53 +0200

    adds tests

--- src/test/java/MinHashTest.java
+++ src/test/java/MinHashTest.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinHashTest {
@@ -14,5 +16,15 @@ public class MinHashTest {
         int minHash = MinHash.of(shingles);
 
         assertThat(minHash).isEqualTo(279080452);
+    }
+
+    @Test
+    public void shouldWorkOnSingleItem() throws Exception {
+        assertThat(MinHash.of(singletonList("foobar"))).isNotEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnZeroForEmptyList() throws Exception {
+        assertThat(MinHash.of(new LinkedList<>())).isEqualTo(0);
     }
 }
