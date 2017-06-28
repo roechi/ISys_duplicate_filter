96eca1e7cd547a0c0a29624cf16a28762bc83ce3
roechi <msg.remus@gmail.com>
2017-06-28 21:56:46 +0200

    Update rest api to use path variables rather than query params

--- src/main/java/isys/duplicatefilter/controllers/ArticleController.java
+++ src/main/java/isys/duplicatefilter/controllers/ArticleController.java
@@ -1,17 +1,17 @@
 package isys.duplicatefilter.controllers;
 
-import isys.duplicatefilter.dto.Article;
+import isys.duplicatefilter.dto.ErrorMessage;
+import isys.duplicatefilter.dto.Pages;
 import isys.duplicatefilter.exceptions.BadRequestException;
 import isys.duplicatefilter.services.ArticleService;
 import isys.duplicatefilter.services.FilteredArticleService;
 import isys.duplicatefilter.services.RawArticleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.assertj.core.util.Preconditions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.Optional;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -30,9 +31,9 @@ public class ArticleController {
     private final FilteredArticleService filteredArticleService;
     private final RawArticleService rawArticleService;
 
-    @GetMapping("/articles")
-    public ResponseEntity getArticles(@RequestParam(required = false) Integer page) {
-        return getResponseEntity(page, rawArticleService);
+    @GetMapping("/articles/page/{pageNumber}")
+    public ResponseEntity getArticles(@PathVariable(required = false) Integer pageNumber) {
+        return getResponseEntity(pageNumber, rawArticleService);
     }
 
     @GetMapping("/duplicates")
@@ -40,30 +41,40 @@ public class ArticleController {
         return new ModelAndView("redirect:/articles?page=1");
     }
 
-    @GetMapping(path = "/filteredArticles")
-    public ResponseEntity getFilteredArticles(@RequestParam(required = false) Integer page) throws IOException, URISyntaxException {
-        return getResponseEntity(page, filteredArticleService);
+    @GetMapping(path = "/filteredArticles/page/{pageNumber}")
+    public ResponseEntity getFilteredArticles(@PathVariable(required = false) Integer pageNumber) throws IOException, URISyntaxException {
+        return getResponseEntity(pageNumber, filteredArticleService);
+    }
+
+    @GetMapping("/{articleType}/pages")
+    public ResponseEntity getNumberOfPages(@PathVariable String articleType) {
+        if (articleType.equals("articles")) {
+            return new ResponseEntity<>(new Pages(rawArticleService.getNumberOfPages()), HttpStatus.OK);
+        }
+        if (articleType.equals("filteredArticles")) {
+            return new ResponseEntity<>(new Pages(rawArticleService.getNumberOfPages()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ErrorMessage("Allowed resources are /articles/pages and /filteredArticles/pages"), HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity getResponseEntity(@RequestParam Integer page, ArticleService<?> service) {
         return Optional.ofNullable(page)
                 .map(pageNumber -> {
-                    Page<? extends Article> all = getPage(pageNumber, service);
-                    return new ResponseEntity<>(newArrayList(all), HttpStatus.OK);
+                    List<?> fetchedPage = getPage(pageNumber, service);
+                    return new ResponseEntity<>(newArrayList(fetchedPage), HttpStatus.OK);
                 }).orElseThrow(() -> {
-                    Page<? extends Article> defaultPage = getPage(1, service);
                     String message = MessageFormat.format(
-                            "There are {0} pages, choose one. {1}",
-                            defaultPage.getTotalPages(),
-                            "Example: GET /articles?page=3 or GET /filteredArticles?page=3"
+                            "There are {0} pages, choose one. Counting starts at 0. {1}. {2}.",
+                            service.getNumberOfPages(),
+                            "Example: GET /articles/page/3 or GET /filteredArticles/page/3",
+                            "To get the number of pages try GET /articles/pages or GET /filteredArticles/pages"
                     );
                     return new BadRequestException(message);
                 });
     }
 
-
-    private Page<? extends Article> getPage(Integer pageNumber, ArticleService repository) {
-        Pageable pageable = new PageRequest(pageNumber, 100);
-        return repository.findAll(pageable);
+    private List getPage(Integer pageNumber, ArticleService service) {
+        Preconditions.checkArgument(pageNumber >= 0 && pageNumber <service.getNumberOfPages(), "Invalid page number " + pageNumber + ".");
+        return service.getPage(pageNumber);
     }
 }
