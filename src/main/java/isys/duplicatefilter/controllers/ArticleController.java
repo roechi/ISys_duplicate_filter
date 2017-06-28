e5ef02d6fea56e04080d4681ce5872be974e10cd
roechi <msg.remus@gmail.com>
2017-06-28 22:15:26 +0200

    Clarify API usage

--- src/main/java/isys/duplicatefilter/controllers/ArticleController.java
+++ src/main/java/isys/duplicatefilter/controllers/ArticleController.java
@@ -2,7 +2,6 @@ package isys.duplicatefilter.controllers;
 
 import isys.duplicatefilter.dto.ErrorMessage;
 import isys.duplicatefilter.dto.Pages;
-import isys.duplicatefilter.exceptions.BadRequestException;
 import isys.duplicatefilter.services.ArticleService;
 import isys.duplicatefilter.services.FilteredArticleService;
 import isys.duplicatefilter.services.RawArticleService;
@@ -12,17 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.text.MessageFormat;
 import java.util.List;
-import java.util.Optional;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,9 +25,27 @@ public class ArticleController {
     private final FilteredArticleService filteredArticleService;
     private final RawArticleService rawArticleService;
 
+    @GetMapping("/articles/page")
+    public ResponseEntity getWithoutPageNumber() {
+        return new ResponseEntity<>(
+                new ErrorMessage("Missing page number. " +
+                        "The number of pages can be found at GET /articles/pages or /filtered/articles/pages. " +
+                        "The first page is 0."),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/filteredArticles/page")
+    public ResponseEntity getFilteredWithoutPageNumber() {
+        return new ResponseEntity<>(
+                new ErrorMessage("Missing page number. " +
+                        "The number of pages can be found at GET /articles/pages or /filtered/articles/pages. " +
+                        "The first page is 0."),
+                HttpStatus.BAD_REQUEST);
+    }
+
     @GetMapping("/articles/page/{pageNumber}")
-    public ResponseEntity getArticles(@PathVariable(required = false) Integer pageNumber) {
-        return getResponseEntity(pageNumber, rawArticleService);
+    public ResponseEntity getArticles(@PathVariable Integer pageNumber) {
+        return retrievePage(pageNumber, rawArticleService);
     }
 
     @GetMapping("/duplicates")
@@ -42,8 +54,8 @@ public class ArticleController {
     }
 
     @GetMapping(path = "/filteredArticles/page/{pageNumber}")
-    public ResponseEntity getFilteredArticles(@PathVariable(required = false) Integer pageNumber) throws IOException, URISyntaxException {
-        return getResponseEntity(pageNumber, filteredArticleService);
+    public ResponseEntity getFilteredArticles(@PathVariable Integer pageNumber) throws IOException, URISyntaxException {
+        return retrievePage(pageNumber, filteredArticleService);
     }
 
     @GetMapping("/{articleType}/pages")
@@ -57,24 +69,9 @@ public class ArticleController {
         return new ResponseEntity<>(new ErrorMessage("Allowed resources are /articles/pages and /filteredArticles/pages"), HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity getResponseEntity(@RequestParam Integer page, ArticleService<?> service) {
-        return Optional.ofNullable(page)
-                .map(pageNumber -> {
-                    List<?> fetchedPage = getPage(pageNumber, service);
-                    return new ResponseEntity<>(newArrayList(fetchedPage), HttpStatus.OK);
-                }).orElseThrow(() -> {
-                    String message = MessageFormat.format(
-                            "There are {0} pages, choose one. Counting starts at 0. {1}. {2}.",
-                            service.getNumberOfPages(),
-                            "Example: GET /articles/page/3 or GET /filteredArticles/page/3",
-                            "To get the number of pages try GET /articles/pages or GET /filteredArticles/pages"
-                    );
-                    return new BadRequestException(message);
-                });
-    }
-
-    private List getPage(Integer pageNumber, ArticleService service) {
+    private ResponseEntity retrievePage(Integer pageNumber, ArticleService<?> service) {
         Preconditions.checkArgument(pageNumber >= 0 && pageNumber <service.getNumberOfPages(), "Invalid page number " + pageNumber + ".");
-        return service.getPage(pageNumber);
+        List<?> page = service.getPage(pageNumber);
+        return new ResponseEntity<>(page, HttpStatus.OK);
     }
 }
