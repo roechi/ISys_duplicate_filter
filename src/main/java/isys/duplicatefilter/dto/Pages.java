14a423c8a12674a6f51fff7a454e23dde2edfed7
roechi <msg.remus@gmail.com>
2017-06-28 21:52:39 +0200

    Add pages dto

---
+++ src/main/java/isys/duplicatefilter/dto/Pages.java
@@ -0,0 +1,12 @@
+package isys.duplicatefilter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pages {
+    private int pages;
+}
